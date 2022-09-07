9a843d4e025704014913005fa84702251a845505
KSJJM <kimpoong88@gmail.com>
2022-09-07 17:44:05 +0900

    draft

--- textminer.py
+++ textminer.py
@@ -1,4 +1,7 @@
 # from unittest import result
+from cgitb import text
+from pydoc import tempfilepager
+from tempfile import tempdir
 import pandas as pd
 # import nltk
 from nltk.tokenize import word_tokenize as wt
@@ -41,20 +44,65 @@ def give_emoji_free_text(text):
     clean_text = ' '.join([str for str in text.split() if not any(i in str for i in emoji_list)])
     return clean_text
 
-df = pd.read_excel("facebookData.xlsx")
-contents = df["postDetail"].to_list()
 
-#stopwords set up
-stopWordList = stopwords.words('french')
-noWord = re.compile("\W")
+def del_stopword(text):
+    stopWordList = ['à','et','en', 'détail', 'contactez', 'nous', 'suis', 'un', 'peu', 'de', 'problème', 'proposez', 'num', ',','!', '?', 'show' ]
+    clean_text = ' '.join([str for str in text.split() if any(i in str for i in stopWordList)])
+    return clean_text
 
-output = pd.DataFrame(columns={'rawData', 'content', 'products', 'battery', 'price'})
-output['content'] = df['postDetail']
-temp = []
-for line in contents:
-    line = give_emoji_free_text(line)
-    line = remove_emoji(line)
-    temp.append(line)
 
-            
-print(temp)
+def get_contents(RAWDATA_PATH):
+    df = pd.read_excel(RAWDATA_PATH)
+    contents = df["postDetail"].to_list()
+
+    #stopwords set up
+
+    temp = []
+    for line in contents:
+        line = line.lower()
+        line = give_emoji_free_text(line)
+        line = remove_emoji(line)
+        temp.append(line)
+
+    out = pd.DataFrame(temp, columns=['content'], dtype='string')
+    out.drop_duplicates(['content'], keep='first', ignore_index=True, inplace= True)
+    out.dropna()
+
+    dic = {}
+    temp = []
+    for line in out['content']:
+        line = line.split(' ')
+        temp.append(line)
+        
+    dic["raw"] = out['content'].to_list()
+    dic["word"] = temp
+
+    out = pd.DataFrame(dic, dtype="object")
+    
+    return out
+
+RAWDATA_PATH = "facebookData.xlsx"
+df = get_contents(RAWDATA_PATH)
+# df.to_csv("facebookData")
+price = []
+product = []
+memory = []
+battery = []
+
+product_list = ['iphone', '5s', '6','6s','6+', '6s+', '7', '7+', '8',' 8+', 'se', 'x', 'xs', 'max', 'xr', '11','pro', 'promax', '12' ,'mini', 'spark', 's16', 's8', 'samsung', 'a36']
+
+for line in df["word"]:
+    price.append([text for text in line if '$' in text])
+    product.append([text for text in line if any(i in text for i in product_list) ])
+    # [text for text in line if any(i in str for i in stopWordList)]
+    memory.append([text for text in line if 'gb' in text])
+    battery.append([text for text in line if '%' in text])
+
+dic = {}
+dic["price"] = price
+dic["product"] = product
+dic["memory"] = memory
+dic["battery"] = battery
+
+df = pd.DataFrame(dic, dtype="object")
+df.to_excel("outputData.xlsx")
