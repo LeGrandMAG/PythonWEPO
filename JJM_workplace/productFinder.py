7a6a6fc823da4517ed645c5b94f7b4c44bc697dc
KSJJM <kimpoong88@gmail.com>
2022-09-25 20:07:29 +0900

    before remover non target!!!

--- JJM_workplace/productFinder.py
+++ JJM_workplace/productFinder.py
@@ -7,53 +7,40 @@ import itertools
 fomalProductNames = """Samsung Galaxy S6
     Samsung Galaxy S7
     Samsung Galaxy S8
-    Samsung Galaxy S8Plus
+    Samsung Galaxy S8 Plus
     Samsung Galaxy S9
-    Samsung Galaxy S9Plus
+    Samsung Galaxy S9 Plus
     Samsung Galaxy S10
-    Samsung Galaxy S10Plus
+    Samsung Galaxy S10 Plus
     Samsung Galaxy S10e
     Samsung Galaxy S10 5G
     Samsung Galaxy S20
-    Samsung Galaxy S20Plus
-    Samsung Galaxy S20Ultra 5G
-    Samsung Galaxy S20Plus 5G
+    Samsung Galaxy S20 Plus
+    Samsung Galaxy S20 Ultra 5G
+    Samsung Galaxy S20 Plus 5G
     Samsung Galaxy S20 5G
-    Samsung Galaxy S20Ultra LTE
+    Samsung Galaxy S20 Ultra LTE
     Samsung Galaxy S20Fe
     Samsung Galaxy S20Fe 5G
     Samsung Galaxy S21 5G
-    Samsung Galaxy S21Plus 5G
-    Samsung Galaxy S21Ultra 5G
+    Samsung Galaxy S21 Plus 5G
+    Samsung Galaxy S21 Ultra 5G
     Samsung Galaxy S21Fe 5G
     Samsung Galaxy S22 5G
-    Samsung Galaxy S22Plus 5G
-    Samsung Galaxy S22Ultra 5G
-    Samsung Galaxy S20Fe 2022
-    Samsung Galaxy A5Duos
-    Samsung Galaxy A5Duos
+    Samsung Galaxy S22 Plus 5G
+    Samsung Galaxy S22 Ultra 5G
+    Samsung Galaxy A5 Duos
     Samsung Galaxy A3
-    Samsung Galaxy A3Duos
+    Samsung Galaxy A3 Duos
     Samsung Galaxy A5
     Samsung Galaxy A7
-    Samsung Galaxy A7Duos
+    Samsung Galaxy A7 Duos
     Samsung Galaxy A8
-    Samsung Galaxy A8Duos
-    Samsung Galaxy A3 2016
-    Samsung Galaxy A5 2016
-    Samsung Galaxy A7 2016
-    Samsung Galaxy A9 2016
-    Samsung Galaxy A9Pro 2016
-    Samsung Galaxy A8 2016
-    Samsung Galaxy A3 2017
-    Samsung Galaxy A5 2017
-    Samsung Galaxy A7 2017
-    Samsung Galaxy A6 2018
-    Samsung Galaxy A6Plus 2018
-    Samsung Galaxy A8Star 
-    Samsung Galaxy A9Star
-    Samsung Galaxy A7 2018
-    Samsung Galaxy A9 2018
+    Samsung Galaxy A8 Duos
+    Samsung Galaxy A9 Pro
+    Samsung Galaxy A6 Plus
+    Samsung Galaxy A8 Star 
+    Samsung Galaxy A9 Star
     Samsung Galaxy A6s
     Samsung Galaxy A8s
     Samsung Galaxy A10
@@ -64,7 +51,7 @@ fomalProductNames = """Samsung Galaxy S6
     Samsung Galaxy A50
     Samsung Galaxy A60
     Samsung Galaxy A70
-    Samsung Galaxy A2Core
+    Samsung Galaxy A2 Core
     Samsung Galaxy A80
     Samsung Galaxy A10e
     Samsung Galaxy A10s
@@ -79,11 +66,11 @@ fomalProductNames = """Samsung Galaxy S6
     Samsung Galaxy A31
     Samsung Galaxy A51 5G
     Samsung Galaxy A41
-    Samsung Galaxy Quantum
+    Samsung Galaxy AQuantum
     Samsung Galaxy A21s
     Samsung Galaxy A71 5G
     Samsung Galaxy A21
-    Samsung Galaxy A01Core
+    Samsung Galaxy A01 Core
     Samsung Galaxy A71Uw 5G 
     Samsung Galaxy A51Uw 5G 
     Samsung Galaxy A42 5G
@@ -97,10 +84,10 @@ fomalProductNames = """Samsung Galaxy S6
     Samsung Galaxy A72
     Samsung Galaxy A22
     Samsung Galaxy A22 5G
-    Samsung Galaxy A12Nacho
+    Samsung Galaxy A12 Nacho
     Samsung Galaxy A03s
     Samsung Galaxy A52s 5G
-    Samsung Galaxy A03Core
+    Samsung Galaxy A03 Core
     Samsung Galaxy A03
     Samsung Galaxy A13 5G
     Samsung Galaxy A13
@@ -108,37 +95,37 @@ fomalProductNames = """Samsung Galaxy S6
     Samsung Galaxy A33 5G
     Samsung Galaxy A53 5G
     Samsung Galaxy A73 5G
-    Samsung Galaxy NotePro 12.2
+    Samsung Galaxy Note Pro 12.2
     Samsung Galaxy Note3Neo
-    Samsung Galaxy Note3NeoDuos
-    Samsung Galaxy NotePro 12.2 5G
-    Samsung Galaxy NotePro 12.2 LTE
+    Samsung Galaxy Note3Neo Duos
+    Samsung Galaxy Note Pro 12.2 5G
+    Samsung Galaxy Note Pro 12.2 LTE
     Samsung Galaxy Note4
-    Samsung Galaxy Note4Duos
+    Samsung Galaxy Note4 Duos
     Samsung Galaxy NoteEdge
-    Samsung Galaxy Note5Duos
+    Samsung Galaxy Note5 Duos
     Samsung Galaxy Note5
     Samsung Galaxy Note7
-    Samsung Galaxy NoteFE
+    Samsung Galaxy NoteFe
     Samsung Galaxy Note8
     Samsung Galaxy Note9
     Samsung Galaxy Note10
     Samsung Galaxy Note10 5G
-    Samsung Galaxy Note10Plus
-    Samsung Galaxy Note10Plus 5G
-    Samsung Galaxy Note10Lite
+    Samsung Galaxy Note10 Plus
+    Samsung Galaxy Note10 Plus 5G
+    Samsung Galaxy Note10 Lite
     Samsung Galaxy Note20
     Samsung Galaxy Note20 5G
-    Samsung Galaxy Note20Ultra
-    Samsung Galaxy Note20Ultra 5G
-    Samsung Galaxy TabPro 8.4
-    Samsung Galaxy Tab3Lite 7.0
-    Samsung Galaxy TabPro 8.4 3G/LTE
-    Samsung Galaxy TabPro 10.1
-    Samsung Galaxy TabPro 10.1 LTE
-    Samsung Galaxy TabPro 12.2
-    Samsung Galaxy TabPro 12.2 3G
-    Samsung Galaxy TabPro 12.2 LTE
+    Samsung Galaxy Note20 Ultra
+    Samsung Galaxy Note20 Ultra 5G
+    Samsung Galaxy Tab Pro 8.4
+    Samsung Galaxy Tab3 Lite 7.0
+    Samsung Galaxy Tab Pro 8.4 3G/LTE
+    Samsung Galaxy Tab Pro 10.1
+    Samsung Galaxy Tab Pro 10.1 LTE
+    Samsung Galaxy Tab Pro 12.2
+    Samsung Galaxy Tab Pro 12.2 3G
+    Samsung Galaxy Tab Pro 12.2 LTE
     Samsung Galaxy Tab3Lite 7.0 3G
     Samsung Galaxy Tab4 7.0 LTE
     Samsung Galaxy Tab4 7.0 3G
@@ -153,46 +140,34 @@ fomalProductNames = """Samsung Galaxy S6
     Samsung Galaxy TabS 10.5 LTE
     Samsung Galaxy TabS 8.4
     Samsung Galaxy TabS 8.4 LTE
-    Samsung Galaxy Tab3Lite 7.0 VE
+    Samsung Galaxy Tab3 Lite 7.0 VE
     Samsung Galaxy Tab3V
     Samsung Galaxy TabA 8.0
     Samsung Galaxy TabA 9.7
     Samsung Galaxy TabA & S Pen
-    Samsung Galaxy Tab4 10.1 2015
     Samsung Galaxy TabE 9.6
     Samsung Galaxy TabS2 8.0
     Samsung Galaxy TabS2 9.7
     Samsung Galaxy TabE 8.0
-    Samsung Galaxy TabA 7.0 2016
-    Samsung Galaxy TabA 10.1 2016
     Samsung Galaxy TabJ
     Samsung Galaxy TabS3 9.7
-    Samsung Galaxy TabA 8.0 2017
     Samsung Galaxy TabActive 2
     Samsung Galaxy TabA 10.5
     Samsung Galaxy TabS4 10.5
-    Samsung Galaxy TabA 8.0 2018
     Samsung Galaxy TabAdvanced2
-    Samsung Galaxy TabA 8.0 & S Pen 2019
-    Samsung Galaxy TabA 10.1 2019
     Samsung Galaxy TabS5e
-    Samsung Galaxy TabA 8.0 2019
     Samsung Galaxy TabS6
-    Samsung Galaxy TabActivePro
+    Samsung Galaxy TabActive Pro
     Samsung Galaxy TabS6 5G
-    Samsung Galaxy TabA 8.4 2020
-    Samsung Galaxy TabS6Lite
+    Samsung Galaxy TabS6 Lite
     Samsung Galaxy TabS7
-    Samsung Galaxy TabS7Plus
-    Samsung Galaxy TabA7 10.4 2020
+    Samsung Galaxy TabS7 Plus
     Samsung Galaxy TabActive3
-    Samsung Galaxy TabS7FE
-    Samsung Galaxy TabA7Lite
-    Samsung Galaxy TabA8 10.5 2021
+    Samsung Galaxy TabS7Fe
+    Samsung Galaxy TabA7 Lite
     Samsung Galaxy TabS8
-    Samsung Galaxy TabS8Plus
-    Samsung Galaxy TabS8Ultra
-    Samsung Galaxy TabS6Lite 2022
+    Samsung Galaxy TabS8 Plus
+    Samsung Galaxy TabS8 Ultra
     Samsung Galaxy Fold
     Samsung Galaxy Fold 5G
     Samsung Galaxy ZFold2 5G
@@ -203,63 +178,63 @@ fomalProductNames = """Samsung Galaxy S6
     Samsung Galaxy ZFold4
     Samsung Galaxy ZFlip4
     Apple iPhone 6
-    Apple iPhone 6Plus
-    Apple iPhone 6S
-    Apple iPhone 6SPlus
+    Apple iPhone 6 Plus
+    Apple iPhone 6s
+    Apple iPhone 6s Plus
     Apple iPhone Se 1st gen
     Apple iPhone 7
-    Apple iPhone 7Plus
+    Apple iPhone 7 Plus
     Apple iPhone 8
-    Apple iPhone 8Plus
+    Apple iPhone 8 Plus
     Apple iPhone X
     Apple iPhone Xr
     Apple iPhone Xs
-    Apple iPhone XsMax
+    Apple iPhone Xs Max
     Apple iPhone 11
-    Apple iPhone 11Pro
-    Apple iPhone 11ProMax
+    Apple iPhone 11 Pro
+    Apple iPhone 11 Pro Max
     Apple iPhone Se 2nd gen
     Apple iPhone 12
-    Apple iPhone 12Mini
-    Apple iPhone 12Pro
-    Apple iPhone 12ProMax
+    Apple iPhone 12 Mini
+    Apple iPhone 12 Pro
+    Apple iPhone 12 Pro Max
     Apple iPhone 13
-    Apple iPhone 13Mini
-    Apple iPhone 13Pro
-    Apple iPhone 13ProMax
+    Apple iPhone 13 Mini
+    Apple iPhone 13 Pro
+    Apple iPhone 13 ProMax
     Apple iPhone Se 3rd gen
     Apple iPhone 14
-    Apple iPhone 14Plus
-    Apple iPhone 14Pro
-    Apple iPhone 14ProMax
-    Tecno SparkPlus
-    Tecno SparkPro
+    Apple iPhone 14 Plus
+    Apple iPhone 14 Pro
+    Apple iPhone 14 Pro Max
+    Tecno Spark Plus
+    Tecno Spark Pro
     Tecno Phantom6
-    Tecno PopLite
-    Tecno Pouvoir2Pro
-    Tecno Phantom6Plus
+    Tecno Pop Lite
+    Tecno Pouvoir2 Pro
+    Tecno Phantom6 Plus
     Tecno Pouvoir
     Tecno Pop
     Tecno SparkCm
     Tecno Spark
     Tecno CamonCm
     Tecno F2 LTE
-    Tecno CamonCxAir
+    Tecno CamonCx Air
     Tecno CamonCx
     Tecno Pouvoir2
-    Tecno PopPro
+    Tecno Pop Pro
     Tecno PopS
     Tecno CamonX
     Tecno Spark2
-    Tecno CamonXPro
+    Tecno CamonX Pro
     Tecno Phantom8
     Tecno Camon11
     Tecno SparkGo
     Tecno Spark8
-    Tecno Spark8Pro
+    Tecno Spark8 Pro
     Tecno F2"""
 
-stop = ["3g/lte", "5g", "lte", "3g" , "samsung", "iphone", "galaxy", "tecno", "apple", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"] # we need to delete manipulated yr data
+stop = ['LTE',] # we need to delete manipulated yr data
 # variarion should be modified by human
 variation = {
     'samsung': ['ss', 'smg', 'ssg', 'sam', 'sm', 'samsung'],
@@ -411,24 +386,56 @@ for text in fomalNameList:
     # 제품 매핑 및 태깅 위한 자료 만들기
     for m in matches:
         modelingrediant.append(m.group().lower()) 
-        # print(modelingrediant)
 
-    if len(modelingrediant) >=1:
-        for ran in range(len(modelingrediant)):
-            for p in itertools.permutations(modelingrediant, ran+1):
-                modelCandidates.append(' '.join(p))
-                modelCandidates.append(''.join(p))
+    if len(modelingrediant) >=2:
+        if modelingrediant[0] == 'a' or modelingrediant[0] == 's':
+            modelingrediant[0] = modelingrediant[0] + modelingrediant[1]
+            modelingrediant = [modelingrediant[0]] + modelingrediant[2:]
+        # print(modelingrediant)
+        
+    for ran in range(len(modelingrediant)):
+        for c in itertools.combinations(modelingrediant, ran+1):
+            modelCandidates.append(' '.join(c))
+            modelCandidates.append(''.join(c))
+
+    modelCandidates = list(set(modelCandidates))
+    
+    # 한글자 짜리 제거  
+    tModelcd = []
+    if len(modelCandidates) > 1:
+        for cd in modelCandidates:
+            if len(cd) > 1:
+                tModelcd.append(cd)
+        modelCandidates = tModelcd
+        
+    
+    # version 조합 만들기
+    tempVer = []
+    for c in itertools.combinations(tempWords[1:], len(tempWords[1:])):
+        tempVer.append(' '.join(c).lower())
+        tempVer.append(''.join(c).lower())
+    
     tempMC = []
-    for mc in modelCandidates:
+    for mc in tempVer:
         mcVar = re.sub('ultra','u', mc)
         tempMC.append(mcVar)
         mcVar = re.sub('pro', 'p', mc)
         tempMC.append(mcVar)
         mcVar = re.sub('plus', '+', mc)
         tempMC.append(mcVar)
-    modelCandidates = list(set(modelCandidates + tempMC))
+    tempVer = list(set(tempVer + tempMC))
+        
+    # print(modelCandidates, tempWords[1:], list(set(tempVer)))
+    
+    modelsForMapping = []
+    for produc in itertools.product(modelCandidates, list(set(tempVer))):
+        modelsForMapping.append(' '.join(produc).strip())
+        modelsForMapping.append(''.join(produc).strip())
+        
+    # print(modelCandidates, list(set(modelsForMapping)))
+    
 
-    productDic[2] = modelCandidates #매핑을 위해 model(2) 자료 넣기
+    productDic[2] = list(set(modelsForMapping)) #매핑을 위해 model(2) 자료 넣기
     if len(tempWords) >=2:
         productDic[3] = [i.lower() for i in tempWords[1:]] #매핑을 위해 version(3) 자료 넣기
     
@@ -436,7 +443,7 @@ for text in fomalNameList:
     
 
 # allocate tag 0=company, 1=brand, 2=model, 3=version
-    tag[2] = tag[2] + modelCandidates
+    tag[2] = tag[2] + modelCandidates + [i.lower() for i in tempWords[1:]] + modelsForMapping
     tag[3] = tag[3] + [i.lower() for i in tempWords[1:]]
 
 tag[2] = list(set(tag[2]))
@@ -444,7 +451,6 @@ tag[3] = list(set(tag[3]))
 
 ## checking point 
 # print(tag)
-# print(productMap["Apple iPhone 12Mini"][1][0])
 # for name in productMap:
 #     if len(productMap[name]) > 1:
 #         print("this is name : ", name)
@@ -533,7 +539,7 @@ for token in productFromDocs:
 
     modelCombination.append(list(set(tempCombi)))
 
-# # check point
+# check point
 # for i in range(len(productFromDocs)):
 #     if len(productFromDocs[i]) >= 2:
 #         productFromDocs[i].sort(key = lambda x : x[1])
@@ -556,11 +562,11 @@ for combi in modelCombination:
                             tempFomal.append(name)
     fomalProductList.append(tempFomal)
 
-# # check point
-# for i in range(len(fomalProductList)):
-#     if len(productFromDocs[i]) >= 2:
-#         productFromDocs[i].sort(key = lambda x : x[1])
-#     print(modelCombination[i], fomalProductList[i])
+# check point
+for i in range(len(fomalProductList)):
+    if len(productFromDocs[i]) >= 2:
+        productFromDocs[i].sort(key = lambda x : x[1])
+    print(changedToFomalname[i], fomalProductList[i])
 
 
 ## 아닌것들 제외하기
